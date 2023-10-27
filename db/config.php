5d178d2751f5be0df550a1762a9f7ec39c25a714
kamaltechie <ezraidi.kamal.04@gmail.com>
2023-10-27 14:42:23 +0200

    DATABASE CONFIGURATION

--- db/config.php
+++ db/config.php
@@ -1,1 +1,19 @@
 <?php
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'ezraidi_trifiro_bertolino';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    public function getConnection() {
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
