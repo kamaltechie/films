4ec342cbaa25355fddd8ec8b226994fb003cb2f6
kamaltechie <ezraidi.kamal.04@gmail.com>
2023-10-31 00:15:20 +0100

    working on public

--- public/account.php
+++ public/account.php
@@ -1,1 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+    <link rel="stylesheet" href=" assets/css/style2.css">
+</head>
+<body>
+<div id="profile-update">
+    <h2>User Profile</h2>
+    <form action="index.php" method="POST">
+        <label for="nom">Nom:</label>
+        <input type="text" name="nom" id="nom" value="User's Nom" required>
+        <br>
+        <label for="prenom">Prenom:</label>
+        <input type="text" name="prenom" id="prenom" value="User's Prenom" required>
+        <br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" value="user@example.com" required>
+        <br>
+        <label for="adresse">Adresse:</label>
+        <input type="text" name="adresse" id="adresse" value="User's Adresse" required>
+        <br>
+        <label for="password">New Password:</label>
+        <input type="password" name="password" id="password" required>
+        <br>
+        <label for="password-confirm">Confirm Password:</label>
+        <input type="password" name="password-confirm" id="password-confirm" required>
+        <br>
+        <input type="submit" name="submit-btn" value="Update Profile">
+    </form>
+</div>
+
+<script>
+    // You can add a script here to toggle the visibility of the form when needed
+</script>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,84 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FilmMarket - Home</title>
+    <link rel="stylesheet" href="assets/css/style.css"> <!-- Link to your CSS file -->
+</head>
+<body>
+<!-- Navbar -->
+<header>
+    <nav>
+        <div class="logo">
+            <img src="assets/images/logo.jpg" alt="FilmMarket Logo">
+        </div>
+        <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Collections</a></li>
+            <li><a href="#">Films</a></li>
+            <li><a href="#">Contact us</a></li>
+        </ul>
+        <div class="user-info">
+            <button class="cart-icon">Cart</button>
+            <a href="authentication/login.php" class="logout"><button class="logout">Logout</button></a>
+            <a href="account.php" class="user-profile"><button class="user-profile" onclick="toggleProfileUpdate()">Profile</button></a>
+        </div>
+    </nav>
+</header>
 
-echo 'Hello to public!';
+<!-- Search and Picture Section -->
+<section class="search-section">
+    <div class="search-container">
+        <div class="search-bar">
+            <input type="text" name="search" id="search" placeholder="Enter keywords...">
+            <button type="submit" class="search-button">search<i class="fa fa-search"></i></button>
+        </div>
+        <h2>Search for Enjoyable Films</h2>
+    </div>
+
+    <div class="picture">
+        <img src="assets/images/films.jpg" alt="Sample Image">
+    </div>
+</section>
+
+
+
+
+<!-- Films Section with Pagination -->
+<section class="films-section">
+    <div class="film-list">
+        <!-- Display films here -->
+    </div>
+    <div class="pagination">
+        <!-- Pagination links here -->
+    </div>
+</section>
+
+<!-- Contact Us Form -->
+<section class="contact-section">
+    <h2>Contact Us</h2>
+    <form>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="message">Message:</label>
+        <textarea id="message" name="message" required></textarea>
+
+        <button type="submit">Submit</button>
+    </form>
+</section>
+
+<!-- Footer -->
+<footer>
+    <!-- Footer content here -->
+</footer>
+</body>
+
+
+</html>
+
